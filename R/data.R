463bf74a2be9b8216cf8c9cc290929f62f2b0bf2
amhcam <39906054+amhcam@users.noreply.github.com>
2022-12-03 23:23:18 +0800

    changes to trendplot

--- R/data.R
+++ R/data.R
@@ -15,3 +15,19 @@
 #' A data frame with 1 row and 5 columns, each column representing a subject
 "ABX_Looming_baseline"
 
+#' Another ABX looming data set supplied on 3 Dec
+#'
+#' An example dataset with the acceptable format
+#'
+#' @format ## `ABX_2`
+#' A data frame with 150 rows (each representing a time interval) and 12 columns, each column from the second column onward representing a subject
+#'
+"ABX_2"
+
+#' average_velocity vectors for ABX_2
+#'
+#' The average velocity vector associated with the ABX_Looming dataset
+#'
+#' @format ## `ABX_2_avg_velocity`
+#' A data frame with 1 row and 12 columns, each column representing a subject
+"ABX_2_avg_velocity"
---
+++ R/trendplot.R
@@ -0,0 +1,27 @@
+#' A quick temporary function used for generating trend plot
+#'
+#' @param data the data matrix, ABX_Looming for example
+#' @param avg_velocity the average velocity vector, ABX_Looming_baseline for example
+#' @export
+#'
+#' @return a time-series line plot with smoothed curves
+trendplot <- function(data, avg_velocity) {
+
+  avg.velocty <- my_iterator(as.numeric(avg_velocity))
+  vdata <- apply(data[,-1], 2, function(x) normalisation(x, avg_velocity = avg.velocty)) %>% as.data.frame() # data normalised by dividing over the subject-specific average velocity and taking log
+  vdata <- cbind(data$time, vdata)
+
+  g_data <- gather(vdata, key = "subjects", value = "log_velocity", 2:ncol(data))
+  # density_p <- ggplot(g_data, aes(x = log_velocity, y = subjects)) + geom_density_ridges() + theme_classic()
+
+  # time series plot
+  colnames(g_data)[1] <- "time"
+  g_data$time <- as.POSIXct(g_data$time, "Asia/Taipei", format = "%H:%M:%OS")
+  p1 <-ggplot(g_data, aes(x = time, y = log_velocity)) + geom_line() + facet_wrap(vars(subjects), ncol=(ncol(data)-1)/2) + stat_smooth() + theme_light() # generate facet plots
+
+  return(p1)
+}
+#' Helper function for normalisation of the data
+normalisation <- function(x, avg_velocity) {
+  x <- log(x/call_iterator(avg_velocity))
+}
